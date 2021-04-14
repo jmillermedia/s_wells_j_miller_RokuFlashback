d0ace0312933d86e5ceca6aa7626c7a7c01011bf
Jon <59712501+jmillermedia@users.noreply.github.com>
2021-04-14 16:45:33 -0400

    added routing with SQL and linking tables to database for genre

--- routes/api.js
+++ routes/api.js
@@ -32,36 +32,6 @@ router.get("/movies", (req, res) => {
     });
 });
 
-router.get("/television", (req, res) => {
-    connect.getConnection(function(err, connection) { // this is a longhand version to connect from mySQL NPM module
-        if (err) throw err; // not connected!
-        // Use the connection
-        connection.query('SELECT * FROM tbl_television', function (error, results) {
-          // When done with the connection, release it.
-            connection.release();
-          // Handle error after the release.
-            if (error) throw error;
-
-            res.json(results);
-        });
-    });
-});
-
-router.get("/music", (req, res) => {
-    connect.getConnection(function(err, connection) { // this is a longhand version to connect from mySQL NPM module
-        if (err) throw err; // not connected!
-        // Use the connection
-        connection.query('SELECT * FROM tbl_music', function (error, results) {
-          // When done with the connection, release it.
-            connection.release();
-          // Handle error after the release.
-            if (error) throw error;
-
-            res.json(results);
-        });
-    });
-});
-
 // dynamic route handler that can accept a parameter.
 // this is equivalent to $)GET_["id"] (req.params.id)
 // you're passing the id via the route: /api/movies/1, api/movies/20, etc.
@@ -70,6 +40,115 @@ router.get("/movies/:id", (req, res) => {
         if (error) throw error;
         console.log("results", results);
 
+        res.json(results);
+    });
+});
+
+// movie genres route
+router.get("/movies/genre/:genre", (req, res) => {
+    connect.query(`SELECT
+    m.*,
+    GROUP_CONCAT(g.genre_name) AS GENRE
+FROM
+    tbl_movies AS m,
+    tbl_genre AS g,
+    tbl_mov_genre AS m_g
+WHERE
+    m.movies_id = m_g.movie_ID AND g.genre_id = m_g.genre_ID
+AND
+	g.genre_name = '${req.params.genre}'
+GROUP BY
+    m.movies_id`, function (error, results) { // this is the shorthand, but functions the same as the longhand above.
+        if (error) throw error;
+        console.log("results", results);
+        res.json(results);
+    });
+});
+
+// get all television entries
+router.get("/television", (req, res) => {
+    connect.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query('SELECT * FROM tbl_television', function (error, results) {
+            connection.release();
+            if (error) throw error;
+            res.json(results);
+        });
+    });
+});
+
+// get television by ID #
+router.get("/television/:id", (req, res) => {
+    connect.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query(`SELECT * FROM tbl_television WHERE ID=${req.params.id}`, function (error, results) {
+            connection.release();
+            if (error) throw error;
+            res.json(results);
+        });
+    });
+});
+
+router.get("/television/genre/:genre", (req, res) => {
+    connect.query(`SELECT
+    t.*,
+    GROUP_CONCAT(g.genre_name) AS GENRE
+FROM
+    tbl_television AS t,
+    tbl_genre AS g,
+    tbl_tel_genre AS t_g
+WHERE
+    t.ID = t_g.television_ID AND g.genre_id = t_g.genre_ID
+AND
+	g.genre_name = '${req.params.genre}'
+GROUP BY
+    t.ID
+`, function (error, results) {
+        if (error) throw error;
+        console.log("results", results);
+        res.json(results);
+    });
+});
+
+router.get("/music", (req, res) => {
+    connect.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query('SELECT * FROM tbl_music', function (error, results) {
+            connection.release();
+            if (error) throw error;
+            res.json(results);
+        });
+    });
+});
+
+router.get("/music/:id", (req, res) => {
+    connect.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query(`SELECT * FROM tbl_music WHERE ID=${req.params.id}`, function (error, results) {
+            connection.release();
+            if (error) throw error;
+            res.json(results);
+        });
+    });
+});
+
+router.get("/music/genre/:genre", (req, res) => {
+    connect.query(`SELECT
+    m.*,
+    GROUP_CONCAT(g.genre_name) AS GENRE
+FROM
+    tbl_music AS m,
+    tbl_musicgenres AS g,
+    tbl_mus_genre AS m_g
+WHERE
+    m.ID = m_g.music_ID AND g.ID = m_g.genre_ID
+AND
+	g.genre_name = '${req.params.genre}'
+GROUP BY
+    m.ID
+    `, function (error, results) {
+        if (error) throw error;
+        console.log("results", results);
         res.json(results);
     });
 });
