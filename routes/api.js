14df80f66671e97b163929e8f04e9affb8fe4ed4
Selena Wells <selena.elise.wells@gmail.com>
2021-04-14 14:00:18 -0400

    connecting front end

--- routes/api.js
+++ routes/api.js
@@ -8,23 +8,23 @@ const connect = require("../config/sqlConfig");
 
 router.get("/", (req, res) => {
     // res.json = echo json encode(...) in PHP
-    res.json({message: "you hit the api route!"});
+    res.json({ message: "you hit the api route!" });
 });
 
 router.get("/users", (req, res) => {
     // run SQL query here
     // res.json(query result here);
-    res.json({message: "all users route"});
+    res.json({ message: "all users route" });
 });
 
 router.get("/movies", (req, res) => {
     connect.getConnection(function(err, connection) { // this is a longhand version to connect from mySQL NPM module
         if (err) throw err; // not connected!
         // Use the connection
-        connection.query('SELECT * FROM tbl_movies', function (error, results) {
-          // When done with the connection, release it.
+        connection.query('SELECT * FROM tbl_movies', function(error, results) {
+            // When done with the connection, release it.
             connection.release();
-          // Handle error after the release.
+            // Handle error after the release.
             if (error) throw error;
 
             res.json(results);
@@ -36,10 +36,10 @@ router.get("/television", (req, res) => {
     connect.getConnection(function(err, connection) { // this is a longhand version to connect from mySQL NPM module
         if (err) throw err; // not connected!
         // Use the connection
-        connection.query('SELECT * FROM tbl_television', function (error, results) {
-          // When done with the connection, release it.
+        connection.query('SELECT * FROM tbl_television', function(error, results) {
+            // When done with the connection, release it.
             connection.release();
-          // Handle error after the release.
+            // Handle error after the release.
             if (error) throw error;
 
             res.json(results);
@@ -51,10 +51,10 @@ router.get("/music", (req, res) => {
     connect.getConnection(function(err, connection) { // this is a longhand version to connect from mySQL NPM module
         if (err) throw err; // not connected!
         // Use the connection
-        connection.query('SELECT * FROM tbl_music', function (error, results) {
-          // When done with the connection, release it.
+        connection.query('SELECT * FROM tbl_music', function(error, results) {
+            // When done with the connection, release it.
             connection.release();
-          // Handle error after the release.
+            // Handle error after the release.
             if (error) throw error;
 
             res.json(results);
@@ -66,7 +66,7 @@ router.get("/music", (req, res) => {
 // this is equivalent to $)GET_["id"] (req.params.id)
 // you're passing the id via the route: /api/movies/1, api/movies/20, etc.
 router.get("/movies/:id", (req, res) => {
-    connect.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function (error, results) { // this is the shorthand, but functions the same as the longhand above.
+    connect.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function(error, results) { // this is the shorthand, but functions the same as the longhand above.
         if (error) throw error;
         console.log("results", results);
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 // this catches ever route request - every time you change your location bar
 app.use((req, res, next) => {
     console.log('incoming request');
-    console.log(process.env.PORT);
+    console.log(port);
 
     // next is the original route request ie. api/users
     next(); // => send the user to the route they requested
