a2e8e7c2403b6d8d912739d395902647f756272c
Jon <59712501+jmillermedia@users.noreply.github.com>
2021-04-15 23:29:55 -0400

    database update

--- config/userConfig.js
+++ config/userConfig.js
@@ -1,7 +1,7 @@
 // this is our SQL connect information
 const sqlCreds = {
     user: "root",
-    password: "",
+    password: "root",
     host: "localhost",
     port: 3306,
     database: "db_roku"
